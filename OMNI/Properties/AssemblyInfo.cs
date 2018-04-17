6452cf194c8f617b1efdfdf0c17fe3f7bfb72b13
Michael Marsh <mmarsh@wccobelt.com>
2018-04-17 14:58:51 -0500

    backing up working copy

--- OMNI/Properties/AssemblyInfo.cs
+++ OMNI/Properties/AssemblyInfo.cs
@@ -45,7 +45,7 @@ using System.Windows;
 // You can specify all the values or you can default the Publish and Build Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("3.1.47.340")]
-[assembly: AssemblyFileVersion("3.1.47.340")]
+[assembly: AssemblyVersion("3.1.65.535")]
+[assembly: AssemblyFileVersion("3.1.65.535")]
 [assembly: NeutralResourcesLanguage("en")]
 
