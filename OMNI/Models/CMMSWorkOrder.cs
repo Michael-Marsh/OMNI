343e2d60a7be83d058b7f12466f15ce2f5af9188
Michael Marsh <mmarsh@wccobelt.com>
2018-11-05 08:34:05 -0600

    4.0.19.188 Fixed the bug with CMMS WO Submission

--- OMNI/Models/CMMSWorkOrder.cs
+++ OMNI/Models/CMMSWorkOrder.cs
@@ -368,7 +368,7 @@ namespace OMNI.Models
                                                         INSERT INTO
                                                         [cmmsworkorder]([Status], [Priority], [Date], [Submitter], [WorkCenter], [Description], [Safety], [Quality], [Production], [CrewMembersAssigned], [RequestedByDate],
                                                         [RequestDateReason], [DateAssigned], [DateCompleted], [MachineDown], [PartsUsed], [AttachedNotes], [Lockout], [Site], [Rush])
-                                                        Values(@p1, @p2, @p3, @p4, @p5, @p6, @p7, @p8, @p9, @p10, @p11, @p12, @p13, @p14, @p15, @p16, @p17, @p18, @p19, @20);
+                                                        Values(@p1, @p2, @p3, @p4, @p5, @p6, @p7, @p8, @p9, @p10, @p11, @p12, @p13, @p14, @p15, @p16, @p17, @p18, @p19, @p20);
                                                         SELECT [WorkOrderNumber] FROM [cmmsworkorder] WHERE [WorkOrderNumber] = @@IDENTITY;", App.SqlConAsync))
                 {
                     cmd.Parameters.AddWithValue("p1", wo.Status.ToString());
--- OMNI/Properties/AssemblyInfo.cs
+++ OMNI/Properties/AssemblyInfo.cs
@@ -45,7 +45,7 @@ using System.Windows;
 // You can specify all the values or you can default the Publish and Build Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("4.0.18.186")]
-[assembly: AssemblyFileVersion("4.0.18.186")]
+[assembly: AssemblyVersion("4.0.19.188")]
+[assembly: AssemblyFileVersion("4.0.19.188")]
 [assembly: NeutralResourcesLanguage("en")]
 
